7fd4ce93e5ff92062283e22337daab38a74fe34d
mahak <118602944+ymahak@users.noreply.github.com>
2024-06-17 12:20:01 +0000

    commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accId = "122"
+let accName = "hello"
+var accEmail = "hekko12@gmail.com"
+accCity = "Delhi"
+let accState; //undefined
+
+
+console.log(accId);
+
+console.table([accId , accEmail , accName,accCity ,accState]);
+
+/*
+Prefer not to use var 
+because of issue in block scope and functional scope 
+const change ni hota , let change hota h
+
+*/
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,3 @@
+"use strict"; //treat all JS code as newer versionrestricted variant of JS and  , Eliminates Some JavaScript Silent Errors
+
+// alert(3+3) //not work as we are using nodejs , not browser
